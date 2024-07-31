ba6afaf3a7768569cf13fd50f46fc33d4aa16108
Paulo Castilho <32399161+castilhera@users.noreply.github.com>
2024-07-30 23:46:36 -0300

    converted methods to expression body style.

--- src/Extensions/DateExtensions.cs
+++ src/Extensions/DateExtensions.cs
@@ -6,37 +6,23 @@ namespace Extensions;
 public static class DateExtensions
 {
     public static DateOnly ToFirstDayOfMonth(this DateOnly dateOnly)
-    {
-        return new DateOnly(dateOnly.Year, dateOnly.Month, 1);
-    }
+        => new(dateOnly.Year, dateOnly.Month, 1);
 
-    public static DateTime ToFirstDayOfMonth(this DateTime dateTime)
-    {
-        return dateTime.AddDays(-dateTime.Day + 1);
-    }
+    public static DateTime ToFirstDayOfMonth(this DateTime dateTime) 
+        => dateTime.AddDays(-dateTime.Day + 1);
 
-    public static DateTimeOffset ToFirstDayOfMonth(this DateTimeOffset dateTimeOffset)
-    {
-        return dateTimeOffset.AddDays(-dateTimeOffset.Day + 1);
-    }
+    public static DateTimeOffset ToFirstDayOfMonth(this DateTimeOffset dateTimeOffset) 
+        => dateTimeOffset.AddDays(-dateTimeOffset.Day + 1);
 
-    public static DateOnly ToLastDayOfMonth(this DateOnly date)
-    {
-        return new DateOnly(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
-    }
+    public static DateOnly ToLastDayOfMonth(this DateOnly date) 
+        => new(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
 
-    public static DateTime ToLastDayOfMonth(this DateTime date)
-    {
-        return date.AddDays(DateTime.DaysInMonth(date.Year, date.Month) - date.Day);
-    }
+    public static DateTime ToLastDayOfMonth(this DateTime date) 
+        => date.AddDays(DateTime.DaysInMonth(date.Year, date.Month) - date.Day);
 
-    public static DateTimeOffset ToLastDayOfMonth(this DateTimeOffset date)
-    {
-        return date.AddDays(DateTime.DaysInMonth(date.Year, date.Month) - date.Day);
-    }
+    public static DateTimeOffset ToLastDayOfMonth(this DateTimeOffset date) 
+        => date.AddDays(DateTime.DaysInMonth(date.Year, date.Month) - date.Day);
 
-    public static int GetIso8601WeekNumber(this DateTime date)
-    {
-        return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-    }
+    public static int GetIso8601WeekNumber(this DateTime date) 
+        => CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
 }
