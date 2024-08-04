6862a063bcc01222d0205a12c6a9460fb9522f6c
Paulo Castilho <32399161+castilhera@users.noreply.github.com>
2024-08-03 22:34:11 -0300

    wrapper to read stream lines async.

---
+++ src/Extensions/StreamExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    public static class StreamExtensions
+    {
+        public static async IAsyncEnumerable<string?> ReadLinesAsync(this Stream stream)
+        {
+            using var reader = new StreamReader(stream);
+
+            while (!reader.EndOfStream)
+                yield return await reader.ReadLineAsync();
+        }
+    }
+}
