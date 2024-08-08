fb5c84f8cef3b9217b4c9412b9076f7f2af676ce
Paulo Castilho <32399161+castilhera@users.noreply.github.com>
2024-08-08 00:00:45 -0300

    new validation helpers

---
+++ src/Helpers/ValidationHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Helpers
+{
+    public static partial class ValidationHelpers
+    {
+        [GeneratedRegex(@"\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2}")]
+        private static partial Regex CnpjRegex();
+
+        [GeneratedRegex(@"\d{3}\.?\d{3}\.?\d{3}-?\d{2}")]
+        private static partial Regex CpfRegex();
+
+        public static bool IsCnpj(string text)
+            => CnpjRegex().IsMatch(text.Trim());
+
+        public static bool IsCpf(string text)
+            => CpfRegex().IsMatch(text.Trim());
+    }
+}
