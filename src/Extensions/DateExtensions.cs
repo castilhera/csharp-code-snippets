a45ade8c9f64edfa4be69275f2d7ca5bd9ca7824
Paulo Castilho <32399161+castilhera@users.noreply.github.com>
2024-08-12 00:32:57 -0300

    feat: implemented IsWeekend() for all date structs.

--- src/Extensions/DateExtensions.cs
+++ src/Extensions/DateExtensions.cs
@@ -5,27 +5,36 @@ namespace Extensions;
 
 public static class DateExtensions
 {
-    public static bool IsWeekend(this DateTime dateTime) 
-        => dateTime.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday;
+    public static bool IsWeekend(this DayOfWeek dayOfWeek)
+        => dayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday;
+
+    public static bool IsWeekend(this DateTime dateTime)
+        => dateTime.DayOfWeek.IsWeekend();
+
+    public static bool IsWeekend(this DateTimeOffset dateTimeOffset)
+        => dateTimeOffset.DayOfWeek.IsWeekend();
+
+    public static bool IsWeekend(this DateOnly dateOnly)
+        => dateOnly.DayOfWeek.IsWeekend();
 
     public static DateOnly ToFirstDayOfMonth(this DateOnly dateOnly)
         => new(dateOnly.Year, dateOnly.Month, 1);
 
-    public static DateTime ToFirstDayOfMonth(this DateTime dateTime) 
+    public static DateTime ToFirstDayOfMonth(this DateTime dateTime)
         => dateTime.AddDays(-dateTime.Day + 1);
 
-    public static DateTimeOffset ToFirstDayOfMonth(this DateTimeOffset dateTimeOffset) 
+    public static DateTimeOffset ToFirstDayOfMonth(this DateTimeOffset dateTimeOffset)
         => dateTimeOffset.AddDays(-dateTimeOffset.Day + 1);
 
-    public static DateOnly ToLastDayOfMonth(this DateOnly date) 
+    public static DateOnly ToLastDayOfMonth(this DateOnly date)
         => new(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
 
-    public static DateTime ToLastDayOfMonth(this DateTime date) 
+    public static DateTime ToLastDayOfMonth(this DateTime date)
         => date.AddDays(DateTime.DaysInMonth(date.Year, date.Month) - date.Day);
 
-    public static DateTimeOffset ToLastDayOfMonth(this DateTimeOffset date) 
+    public static DateTimeOffset ToLastDayOfMonth(this DateTimeOffset date)
         => date.AddDays(DateTime.DaysInMonth(date.Year, date.Month) - date.Day);
 
-    public static int GetIso8601WeekNumber(this DateTime date) 
+    public static int GetIso8601WeekNumber(this DateTime date)
         => CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
 }
