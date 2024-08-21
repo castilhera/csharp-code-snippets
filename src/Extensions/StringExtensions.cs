aaad42b624513f8d426806e0cf9be8c70b79b504
Paulo Castilho <32399161+castilhera@users.noreply.github.com>
2024-08-20 21:15:19 -0300

    feat: convert string to snake_case

--- src/Extensions/StringExtensions.cs
+++ src/Extensions/StringExtensions.cs
@@ -1,13 +1,55 @@
-﻿using System;
+﻿using System.Text;
 
 namespace Extensions;
 
 public static class StringExtensions
 {
-    public static string Reverse(this string input)
+    public static string Reverse(this string? input)
     {
+        if (string.IsNullOrEmpty(input))
+            return string.Empty;
+
         char[] inputAsArray = input.ToCharArray();
         Array.Reverse(inputAsArray);
         return new string(inputAsArray);
+    }
+
+    public static string ToSnakeCase(this string? input, bool screaming = false)
+    {
+        if (string.IsNullOrEmpty(input))
+            return string.Empty;
+
+        var stringBuilder = new StringBuilder();
+
+        char previousChar = '\0';
+
+        foreach (char currentChar in input)
+        {
+            if (char.IsLetterOrDigit(currentChar))
+            {
+                if (char.IsUpper(currentChar)
+                    && (char.IsLower(previousChar) || char.IsDigit(previousChar)))
+                {
+                    stringBuilder.Append('_');
+                }
+
+                stringBuilder.Append(screaming ?
+                    char.ToUpper(currentChar) : 
+                    char.ToLower(currentChar));
+
+                previousChar = currentChar;
+            }
+            else if (char.IsSeparator(currentChar)
+                     && char.IsLetterOrDigit(previousChar))
+            {
+                stringBuilder.Append('_');
+                previousChar = currentChar;
+            }
+        }
+
+        if(char.IsSeparator(previousChar))
+            stringBuilder.Length--;
+
+        return stringBuilder.ToString();
     }
 }
