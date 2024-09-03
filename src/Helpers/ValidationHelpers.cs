73a9faa9e1503b05238dd3e7f8bd8e56a7e5f584
Paulo Castilho <32399161+castilhera@users.noreply.github.com>
2024-09-02 22:29:19 -0300

    fix: removed .trim() and added ^$ to regex

--- src/Helpers/ValidationHelpers.cs
+++ src/Helpers/ValidationHelpers.cs
@@ -4,16 +4,16 @@ namespace Helpers
 {
     public static partial class ValidationHelpers
     {
-        [GeneratedRegex(@"\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2}")]
+        [GeneratedRegex(@"^\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2}$")]
         private static partial Regex CnpjRegex();
 
-        [GeneratedRegex(@"\d{3}\.?\d{3}\.?\d{3}-?\d{2}")]
+        [GeneratedRegex(@"^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$")]
         private static partial Regex CpfRegex();
 
         public static bool IsValidCnpj(string str)
-            => CnpjRegex().IsMatch(str.Trim());
+            => CnpjRegex().IsMatch(str);
 
         public static bool IsValidCpf(string str)
-            => CpfRegex().IsMatch(str.Trim());
+            => CpfRegex().IsMatch(str);
     }
 }
